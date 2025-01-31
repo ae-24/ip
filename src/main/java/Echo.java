3bd1c3dd0016c963408603c7a29dec27f6ecca62
ae-24 <115100656+ae-24@users.noreply.github.com>
2025-01-31 16:26:19 +0800

    Add task list and 'list' command to display tasks

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -5,6 +5,8 @@ public class Echo {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int listIndex = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Echo");
@@ -13,18 +15,31 @@ public class Echo {
         System.out.println("____________________________________________________________");
 
         while (true) {
-            line = in.nextLine();
+            line = in.nextLine().trim();
 
             if (line.equalsIgnoreCase("bye")) {
                 break;
+            } else if (line.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                if (listIndex == 0) {
+                    System.out.println("No tasks added");
+                } else {
+                    for (int i = 0; i < listIndex; i++) {
+                        System.out.println(taskList[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+                continue;
             }
             System.out.println("____________________________________________________________");
-            System.out.println(line);
+            System.out.println("added: " + line);
             System.out.println("____________________________________________________________");
+            taskList[listIndex] = (listIndex + 1) + ". " + line;
+            listIndex++;
 
         }
 
-
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
