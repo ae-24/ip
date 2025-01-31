77dfda6d9b4915630d8529d6751ba487ff3acf77
ae-24 <115100656+ae-24@users.noreply.github.com>
2025-01-31 16:04:20 +0800

    Add echo functionality and 'bye' exit command

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
+
 public class Echo {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Echo");
         System.out.println("What can I do for you?");
+        System.out.println("Type 'bye' to exit.");
         System.out.println("____________________________________________________________");
+
+        while (true) {
+            line = in.nextLine();
+
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+
+        }
+
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
